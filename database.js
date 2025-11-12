53d45b10b5fb6056f2e2678063ff06439cd107e6
PuntiNugget <pbura@students.wcpss.net>
2025-11-12 13:38:50 -0500

    Update database.js

--- database.js
+++ database.js
@@ -1,1 +1,22 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./users.db');
 
+db.serialize(() => {
+    console.log('Initializing database...');
+    db.run(`
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT UNIQUE NOT NULL,
+            password_hash TEXT NOT NULL,
+            expires_at DATETIME
+        )
+    `, (err) => {
+        if (err) {
+            console.error("Error creating users table:", err.message);
+        } else {
+            console.log("Users table ready.");
+        }
+    });
+});
+
+module.exports = db;
